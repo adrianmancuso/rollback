f8f4283d5e3e9d6a171e1e55c86cf4bd5dc63981
adrian mancuso <adrian.mancuso@gmail.com>
2017-07-26 09:32:16 +1000

    start seed file

--- store_name_scraper.rb
+++ store_name_scraper.rb
@@ -16,76 +16,22 @@ def store_scraper url
 	  agent.user_agent_alias = 'Windows Chrome'
 	}
 	
+	#if target is changed both get target and destination must be changed
 	a.get("https://whatson.melbourne.vic.gov.au" + destination) do |page|
+  	#h4 is specific to whatson.melbourne
   	results = page.search(".//h4")
   	results.each do |store|
   		@store_names.push(store.text)
   	end
   	page.links.each do |link|
   		if link.text == '>'
+  			# recursive call
   			 store_scraper(link.href)
   		end
   	end
-  	binding.pry
   return @store_names
+	end
 end
-end
-
-
-# def find_return_window store_name
-	
-# 	#set agent
-# 	a = Mechanize.new { |agent|
-# 	  agent.user_agent_alias = 'Windows Chrome'
-# 	}
-	
-# 	#search for store on aus Google
-# 	a.get('http://google.com.au/') do |page|
-#   search_result = page.form_with(:id => 'tsf') do |search|
-#     search.q = store_name + ' melbourne clothes'
-#   end.submit
-
-#   #retrieve first link
-# 	links = search_result.search(".//h3[@class='r']")
-# 	@first_hit = links[0].children[0].attributes['href'].value
-# 	end
-
-
-# 	a.get(@first_hit) do |page|
-# 		page.links.each do |link|
-# 			#check for returns in link
-# 			if link.text.downcase.include? 'return'
-#   			a.get(link.href).search(".//p").each do |paragraph|
-#   				if paragraph.text.downcase.include? 'days'
-#   					array = paragraph.text.split(' ')
-#   					i = array.find_index('days')
-#   					preceeding_word = array[i-1]
-# 						if preceeding_word.to_i > 7
-# 							return store_name + ":" +preceeding_word
-# 						end
-#   				end
-#   			end 
-#   			return
-# 			end
-# 		end
-# 	end
-# 	#failing first test, check for customer in links
-# 	if link.text.downcase.include? 'customer'
-# 		a.get(link.href).search(".//p").each do |paragraph|
-# 			if paragraph.text.downcase.include? 'days'
-# 				array = paragraph.text.split(' ')
-# 				i = array.find_index('days')
-# 				preceeding_word = array[i-1]
-# 				if preceeding_word.to_i > 7
-# 					return store_name + ":" +preceeding_word
-# 				end
-# 			end
-# 		end
-# 	return
-# 	end			
-# end
-# end
-
 
 
 store_scraper start
